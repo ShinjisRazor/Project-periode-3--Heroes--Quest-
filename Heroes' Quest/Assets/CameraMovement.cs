32728277706225b5fc4f7a8fe35e742e359560a0
ShannonVanGinkel <shannonvanginkel@gmail.com>
2022-04-06 20:01:27 +0200

    omgeving en iets betere camera

--- Heroes' Quest/Assets/CameraMovement.cs
+++ Heroes' Quest/Assets/CameraMovement.cs
@@ -7,12 +7,14 @@ public class CameraMovement : MonoBehaviour
     public Transform player;
     public Vector3 v3;
     public float distance;
+    public float height;
 
     // Update is called once per frame
     void Update()
     {
         v3.x = player.position.x;
         v3.z = distance;
+        v3.y = height;
         transform.position = v3;
     }
 }
